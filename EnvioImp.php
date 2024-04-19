9bf40d6f5270f1f86cbffd490e311f1588d45f1c
jemjia404 <165838985+jemjia404@users.noreply.github.com>
2024-04-19 10:49:43 -0600

    Update EnvioImp.php

--- EnvioImp.php
+++ EnvioImp.php
@@ -63,60 +63,6 @@ Fecha de emision: $dia <br/>
 <div  class='footer' style='padding:5%'></div>
 </body>
 ";
-$bodyE = "
-    <head>   
-    <style>  
-    .footer {
-    padding: 2%;
-    color: white;
-    padding-left: 4%;
-    background-color: black;
-    }
-    .footext:hover {
-    color: rgb(227, 141, 237);
-    }
-    .p4 {
-    font-family: 'Lucida Calligraphy',cursive;
-    }
-    .p3{
-    font-family: Tahoma, Verdana, sans-serif;
-    }
- </style>
- </head>
-<body >
-<div  style='background-color:blue;padding:2%;'> 
-<p class='p4' style='color:white; text-align:center;padding-top:3%;font-size:5vw'>Pcb De México S De Rl Mi </p>
-</div>
-<div style=padding-left:20% >
-<h1 class='p4' >Cotizacion de PCB desde pagina WEB </h1>
-<p  class='p3' style='font-size:medium'>
-Usuario:<b>$NombrePCB</b><br/>
-Correo:<b> $EmailPCB</b><br/>
-Empresa:<b> $EmpresaPCB</b><br/>
-Telefono: <b>$TelefonoPCB </b><br/> 
-Cantidad: <b>$CantidadPCB</b><br/> 
-Material:<b>$MaterialPCB</b> <br/>
-Color de Mascara:<b> $ColorMAPCB</b> <br/> 
-Serigrafia: <b>$SerigrafiaPCB</b> <br/> 
-Numero de caras:<b> $NCarasPCB </b> <br/>
-Espesor de material:<b> $EspesorPCB </b><br/> 
-Onzas de cobre:<b> $Onzas</b> <br/> 
-PTH: <b>$PTHPCB</b> <br/> 
-Dimension en eje X: <b>$DXPCB $UnidadesPCB </b><br/>
-Dimension en eje Y: <b>$DYPCB $UnidadesPCB</b> <br/>
- Acabados: <b>$AcabadosPCB </b><br/>
-Cantidad de diametro de brocas:<b> $CantidadDDPCB</b><br/>
- Cantidad de Perforaciones: <b> $CantidadDPPCB</b> <br/> 
- Apartado de Extras:<b> $ExtrasPCB </b> <
- Fecha de emision: $dia<br/>
- </p><br/>
-</div>
-<br/>
-<p> Prueba de envio de correo desde puerto 465 protocolo SMTPS para paginas de encriptado HTTPS con soporte para charset utf-8 configuraccion cpnanel de direccion@pcbdemexico.com.mx</p>
- <div class='footer' style='padding:5%'> 
- </div>   
- </body>
-";
 $mailer->isSMTP();
 $mailer->SMTPDebug = 0;
 $mailer->Host = 'mail.pcbdemexico.com.mx';
